3829539d9989c254e96e48cd73982b2c6af7ba1b
Nico Ritsche <nico@pd9e08fd7.dip.t-dialin.net>
2009-11-25 21:11:56 +0100

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :authenticate
+
+  # simple authentication to protect the live development site
+  def authenticate
+    #session[:user_id] = User.find_by_email('nico.ritsche@gmail.com').id
+    return unless ENV['RAILS_ENV'] == 'production'
+    authenticate_or_request_with_http_basic do |id, password|
+      id == 'upload' && password == 'upl0adn0w'
+    end
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
