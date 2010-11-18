53cb768e36222517fb48965c4bf0c138ad17346d
Nico Ritsche <nico@ip191.pelto9.opintanner.fi>
2010-11-18 20:21:26 +0200

    added .gems

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-  config.gem 'haml'
+  config.gem 'haml', :version => '>=3.0.23'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
